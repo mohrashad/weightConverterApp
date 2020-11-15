0abc6b0252b22c86350e93c8ecd9f53183f8e3a9
unknown <mido10mido45@gmail.com>
2020-11-15 19:09:20 +0200

    this app about converting from bound to grams, kilograms and ounce

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// select the outputs container
+const outputContainer = document.getElementById('output');
+
+// display results in documnet function
+const elementsContent = (id, conetent) => document.getElementById(id).textContent = conetent;
+
+// hide the outputs container
+outputContainer.style.visibility = 'hidden';
+
+// handle input event to input field
+document.getElementById('lbsInput').addEventListener('input', e => {
+
+    // appear the outputs container
+    outputContainer.style.visibility = 'visible';
+
+    // get the input field value
+    let lbs = e.target.value;
+
+    // display results in documnet
+    elementsContent('gramsOutput', lbs / 0.0022046);
+    elementsContent('kgOutput', lbs / 2.2046);
+    elementsContent('ozOutput', lbs * 16);
+});
